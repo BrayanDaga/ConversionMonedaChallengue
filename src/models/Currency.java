d0fb63e039b273b1fb16861a955351fa62846d44
Brayan Vilchez Daga <brayandaga@gmail.com>
2023-08-15 20:18:46 +0200

    Acabando la conversion y mensajes

---
+++ src/models/Currency.java
@@ -0,0 +1,44 @@
+package models;
+
+/**
+ * @author Brayan Esta clase es de moneda
+ */
+
+public class Currency {
+	private double valueCoin;
+	private String sign;
+	private String name;
+
+
+	public Currency(double valueCoin, String sign, String name) {
+		this.valueCoin = valueCoin;
+		this.sign = sign;
+		this.name = name;
+	}
+
+	public double getValueCoin() {
+		return valueCoin;
+	}
+
+	public void setValueCoin(double valueCoin) {
+		this.valueCoin = valueCoin;
+	}
+
+	public   double multiplyCoin(double value) {
+		return this.getValueCoin() * value;
+	}
+
+	public  double divideCoin(double value) {
+		return this.getValueCoin() / value;
+	}
+
+	public String messageConverted(double value) {
+		return "Tienes " + this.sign + value + " " + this.name;
+	}
+
+	public String messageConvertedToSoles(double value)
+	{
+		return "Tienes S/. " + value + " Soles";
+	}
+
+}
--- src/views/launcher.java
+++ src/views/launcher.java
@@ -1,46 +1,68 @@
 package views;
 
+
 import javax.swing.JOptionPane;
 
+import models.Currency;
 
-public class launcher {
 
+public class Launcher {
+
+	final static Currency dolar = new Currency(0.27, "$", "Dólares");
+	static Currency euro = new Currency(0.25, "€", "Euros");
+	static Currency libra = new Currency(0.21, "£", "Libras");
+	static Currency won = new Currency(39.32, "₩", "Won Coreano");
+	static Currency yen = new Currency(361.01, "¥", "Yens");
+
+
+	final static Currency[] currencies = new Currency[] { dolar,euro,libra,won,yen};
+
+
+	final static String[] currencyOptions = { "De Soles a Dólar", "De Soles a Euro", "De Soles a Libras",
+			"De Soles a Yen", "De soles a Won Coreano", "De Dólar a Soles", "De Euro a Soles", "De Libras a Soles",
+			"De Yen a Soles", "De Won Coreano a Soles" };
+
+	final static String[] optionsMain = { "Conversor de Moneda", "Conversion de Temperatura" };
+
+	/**
+	 * Toda la logica esta aqui
+	 */
 	static void initLauncher() {
-		String[] options = { "Conversor de Moneda", "Conversion de Temperatura" };
-
-		String optionSelMain = showOptionsDialog("Seleccione una opción de conversión", "Menu", options);
+		/** Inicia un componente JOptionPane con las opciones de que deeas convertir */
+		String optionSelMain = showOptionsDialog("Seleccione una opción de conversión", "Menu", optionsMain);
 
 		if (optionSelMain != null) {
 			switch (optionSelMain) {
 			case "Conversor de Moneda":
-
 				handleCurrencyConversion();
-
 				break;
 			case "Conversion de Temperatura":
 				JOptionPane.showMessageDialog(null, "En desarrollo...");
 				break;
-			default:
-				throw new IllegalArgumentException("Unexpected value: " + optionSelMain);
+
 			}
+		}else {
+			exitProgram();
 		}
 	}
 
+	/**
+	 * Componente para crear una alerta con opciones
+	 * @param message cuerpo del componente
+	 * @param title titulo del componente
+	 * @param options String[] opciones que se enviara para rellenar el componente
+	 * @return retorna el componente
+	 */
 	static String showOptionsDialog(String message, String title, String[] options) {
 		return (String) JOptionPane.showInputDialog(null, message, title, JOptionPane.PLAIN_MESSAGE, null, options,
 				null);
 	}
 
-	final static Double[] solesConvA = new Double[] { 0.27, 0.25, 0.21, 39.32, 361.01 };
 
-	final static String[] currencyOptions = { "De Soles a Dólar", "De Soles a Euro", "De Soles a Libras", "De Soles a Yen",
-			"De soles a Won Coreano", "De Dólar a Soles", "De Euro a Soles", "De Libras a Soles", "De Yen a Soles",
-			"De Won Coreano a Soles" };
-
-
+/**
+ * Inicia un componente JOptionPane con las opciones de la moneda
+ */
 	static void handleCurrencyConversion() {
-
-
 		String optionCurrency = showOptionsDialog("Elija la moneda a que deseas convertir tu dinero", null,
 				currencyOptions);
 		if (optionCurrency != null) {
@@ -49,89 +71,101 @@ public class launcher {
 			} catch (Exception e) {
 				e.printStackTrace();
 			}
+		}else {
+			exitProgram();
 		}
 	}
 
-	static void convertCurrency(String currencyOption)  {
+	/**
+	 * Funcion para convertir el dato ingresao
+	 * @param currencyOption tipo String que es la opcion que se escogio en el tipo de moneda
+	 */
+	static void convertCurrency(String currencyOption) {
 		String optinSelected = JOptionPane.showInputDialog("Ingresa la cantidad de dinero que deseas convertir:");
 
 		Double currentCurrency = (double) 0;
 		try {
 			currentCurrency = Double.valueOf(optinSelected);
 			String message = "";
-			Double valueConverted  = (double) 0;
-			if(currencyOption.equals(currencyOptions[0])) {
-				valueConverted =  (currentCurrency * solesConvA[0]);
-				message = "Tienes $"+ valueConverted + " Dolares";
-			}
-			if(currencyOption.equals(currencyOptions[1])) {
-				valueConverted =   (currentCurrency * solesConvA[1]);
-				message = "Tienes $"+  valueConverted + " Euros";
-			}
-			if(currencyOption.equals(currencyOptions[2])) {
-				valueConverted =   (currentCurrency * solesConvA[2]);
-				message = "Tienes $"+ valueConverted + " Libras";
-			}
-			if(currencyOption.equals(currencyOptions[3])) {
-				valueConverted =  (currentCurrency * solesConvA[3]);
+			Double valueConverted = (double) 0;
 
-				message = "Tienes $"+ valueConverted+ " Yens";
+			/**A partir de que ocpion se escoja se hara la conversion**/
+			/*De Soles a Euros*/
+			if (currencyOption.equals(currencyOptions[0])) {
+				valueConverted =  dolar.multiplyCoin(currentCurrency);
+				message = dolar.messageConverted(valueConverted);
 			}
-			if(currencyOption.equals(currencyOptions[4])) {
-				valueConverted =  (currentCurrency * solesConvA[4]);
-
-				message = "Tienes $"+ valueConverted + " Won Coreano";
-
+			//De Soles a Euros
+			if (currencyOption.equals(currencyOptions[1])) {
+				valueConverted =  euro.multiplyCoin(currentCurrency);
+				message = euro.messageConverted(valueConverted);
 			}
-			if(currencyOption.equals(currencyOptions[5])) {
-				valueConverted =  (currentCurrency / solesConvA[0]);
-				message = "Tienes S/"+ valueConverted + " Soles";
-
+			// De Soles a Libras
+			if (currencyOption.equals(currencyOptions[2])) {
+				valueConverted =  libra.multiplyCoin(currentCurrency);
+				message = libra.messageConverted(valueConverted);
 			}
-			if(currencyOption.equals(currencyOptions[6])) {
-				valueConverted =  (currentCurrency / solesConvA[1]);
-				message = "Tienes S/"+ valueConverted + " Soles";
-
-
+			//De Soles a Won
+			if (currencyOption.equals(currencyOptions[3])) {
+				valueConverted = won.multiplyCoin(currentCurrency);
+				message = won.messageConverted(valueConverted);
 			}
-			if(currencyOption.equals(currencyOptions[7])) {
-				valueConverted =  (currentCurrency / solesConvA[2]);
-				message = "Tienes S/"+ valueConverted + " Soles";
+			//De soles a Yen
+			if (currencyOption.equals(currencyOptions[4])) {
+				valueConverted =  dolar.multiplyCoin(currentCurrency);
+				message = dolar.messageConverted(valueConverted);
 			}
-			if(currencyOption.equals(currencyOptions[8])) {
-				valueConverted =   (currentCurrency / solesConvA[3]);
-				message = "Tienes S/"+ valueConverted + " Soles";
+			//De Dolares a Soles
+			if (currencyOption.equals(currencyOptions[5])) {
+				valueConverted =  dolar.divideCoin(currentCurrency);
+				message = dolar.messageConvertedToSoles(valueConverted);
 
 			}
-			if(currencyOption.equals(currencyOptions[9])) {
-				valueConverted =  (currentCurrency / solesConvA[4]);
-				message = "Tienes S/"+ valueConverted + " Soles";
+			//De Euro a Soles
+			if (currencyOption.equals(currencyOptions[6])) {
+				valueConverted =  euro.divideCoin(currentCurrency);
+				message = euro.messageConvertedToSoles(valueConverted);
+
+			}
+			//De Libras a Soles
+			if (currencyOption.equals(currencyOptions[7])) {
+				valueConverted =  libra.divideCoin(currentCurrency);
+				message = libra.messageConvertedToSoles(valueConverted);
+			}
+			//De Won a Soles
+
+			if (currencyOption.equals(currencyOptions[8])) {
+				valueConverted =  won.divideCoin(currentCurrency);
+				message = won.messageConvertedToSoles(valueConverted);
+			}
+			//De Yen a Soles
+			if (currencyOption.equals(currencyOptions[9])) {
+				valueConverted =  yen.divideCoin(currentCurrency);
+				message = yen.messageConvertedToSoles(valueConverted);
 			}
 
-			getCurrencyMessage(message);
+			JOptionPane.showMessageDialog(null, message);
+
 		} catch (Exception e) {
 			JOptionPane.showMessageDialog(null, "Valor no válido");
-		}
-		finally {
-			int canContinue =  JOptionPane.showConfirmDialog(null, "Deseas continuar?");
-			if(canContinue == 0) {
+		} finally {
+			int canContinue = JOptionPane.showConfirmDialog(null, "Deseas continuar?");
+			if (canContinue == 0) {
 				initLauncher();
-			}else {
-				JOptionPane.showMessageDialog(null, "Programa terminado");
+			} else {
+				exitProgram();
 			}
 		}
-
 
 	}
 
-	public static void getCurrencyMessage(String message) {
-		JOptionPane.showMessageDialog(null, "Tienes " + message);
+	public static  void exitProgram() {
+		JOptionPane.showMessageDialog(null, "Programa terminado");
+		System.exit(1);
 	}
 
 	public static void main(String[] args) {
-
 		initLauncher();
-
 
 	}
 }
